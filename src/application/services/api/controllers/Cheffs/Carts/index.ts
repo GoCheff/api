e08790b132286936fdb3c40dbe664f49cb0639d3
Vinicius <vinigugelmin@gmail.com>
2023-07-29 16:55:20 -0300

    fix: CheffCartsController index imports fixed

--- src/application/services/api/controllers/Cheffs/Carts/index.ts
+++ src/application/services/api/controllers/Cheffs/Carts/index.ts
@@ -1,8 +1,10 @@
 import { CheffCartsController } from "./CheffCartsController";
 import { getAllSentCartsFromCheffUseCase } from "../../../../../../useCases/GetAllSentCartsFromCheff";
+import { approveCustomerCartUseCase } from "../../../../../../useCases/ApproveCustomerCart";
 
 const cheffCartsController = new CheffCartsController(
-  getAllSentCartsFromCheffUseCase
+  getAllSentCartsFromCheffUseCase,
+  approveCustomerCartUseCase
 );
 
 export { cheffCartsController };
