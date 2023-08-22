9af8d0d9da31fe0c511431aff9ba6c52d8f06b06
Vinicius <vinigugelmin@gmail.com>
2023-08-22 20:17:54 -0300

    fix: import fix

--- src/application/services/api/controllers/Customers/Cheffs/index.ts
+++ src/application/services/api/controllers/Customers/Cheffs/index.ts
@@ -1,6 +1,10 @@
 import { CustomerCheffsController } from "./CustomerCheffsController";
 import { getCheffUseCase } from "../../../../../../useCases/GetCheff";
+import { getAllCheffsUseCase } from "../../../../../../useCases/GetAllCheffs";
 
-const customerCheffsController = new CustomerCheffsController(getCheffUseCase);
+const customerCheffsController = new CustomerCheffsController(
+  getCheffUseCase,
+  getAllCheffsUseCase
+);
 
 export { customerCheffsController };
