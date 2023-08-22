e9dce9dd909816f30933bce4f6ff0026bc7f65d0
Vinicius <vinigugelmin@gmail.com>
2023-08-22 16:43:06 -0300

    cd: pipeline update

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM node:18.13
 WORKDIR /app
 COPY . .
 RUN npm i
+RUN npx prisma migrate deploy
 RUN npm run build
 RUN rm -rf src
 RUN npm prune --production
---
+++ scripts/deploy.js
@@ -0,0 +1,26 @@
+const child_process = require("child_process");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const { execSync } = child_process;
+
+const username = process.env.DROPLET_USERNAME;
+const ip = process.env.DROPLET_IP;
+const sshKeyPath = process.env.SSH_KEY_PATH;
+
+const commands = [
+  `ssh -i ${sshKeyPath} ${username}@${ip} "sudo rm -r /var/www/docker/api"`,
+  `ssh -i ${sshKeyPath} ${username}@${ip} "git clone git@github.com:GoCheff/api.git /var/www/docker/api"`,
+  `scp -i ${sshKeyPath} -r .env ${username}@${ip}:/var/www/docker/api`
+];
+
+const deployCommand = commands.join(" && ");
+
+try {
+  execSync(deployCommand, { stdio: "inherit" });
+  console.log("Deploy concluído com sucesso!");
+} catch (error) {
+  console.error("Erro ao executar o deploy:", error.message);
+  process.exit(1);
+}
