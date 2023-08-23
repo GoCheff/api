a183ed0bd6fbe85128f73cd52aacae208d8bbc4b
Vinicius <vinigugelmin@gmail.com>
2023-08-22 22:18:53 -0300

    cd: code changes to apply to prod and prod-dev

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -16,13 +16,15 @@ const username = process.env.DROPLET_USERNAME;
 const ip = process.env.DROPLET_IP;
 const sshKeyPath = process.env.SSH_KEY_PATH;
 const dropletPath = process.env.DROPLET_PATH;
+const dockerComposeName =
+  mode === "production" ? "docker-compose.yml" : "docker-compose.prod-dev.yml";
 
 const commands = [
   `ssh -i ${sshKeyPath} ${username}@${ip} "sudo rm -r ${dropletPath}"`,
   `ssh -i ${sshKeyPath} ${username}@${ip} "git clone git@github.com:GoCheff/api.git ${dropletPath}"`,
   `ssh -i ${sshKeyPath} ${username}@${ip} "cd ${dropletPath} && rm -rf docs && rm -rf .husky && rm -rf scripts"`,
   `scp -i ${sshKeyPath} -r .env ${username}@${ip}:${dropletPath}`,
-  `ssh -i ${sshKeyPath} ${username}@${ip} "cd ${dropletPath} && docker compose up -d --build -f docker-compose.prod-dev.yml"`,
+  `ssh -i ${sshKeyPath} ${username}@${ip} "cd ${dropletPath} && docker compose -f ${dockerComposeName} up -d --build"`,
   `ssh -i ${sshKeyPath} ${username}@${ip} "docker image prune -f"`
 ];
 
